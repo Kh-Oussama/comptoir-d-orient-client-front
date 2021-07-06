2f499db2280dcd50274dd8d2edf9d004922f4dd7
k-Oussama <oussama.khirat@univ-constantine2.dz>
2021-07-06 14:58:44 -0700

    address section is complteted

---
+++ src/components/address-section/address-section.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const AddressSection = () => {
+    return (
+        <div className="address-section">
+            <div className="section-header">
+                <p className="p-before-title">
+                    How do we Work
+                </p>
+                <h2 className="title">
+                    Address Social
+                </h2>
+
+                <p className="title-description">
+                    21 Avenue Lefèvre, 69120 Vaulx-en-Velin
+                </p>
+                <div className="red-divider"/>
+            </div>
+            <div className="map-block">
+                <div className="leaflet-container">
+                    <MapContainer center={[45.77414, 4.91158]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[45.77414, 4.91158]}>
+                            <Popup>
+                                <div className="markerBlock">
+                                    <img src="/images/f4.jpg" alt="image" className="marker-image"/>
+                                    <p>A21 Avenue Lefèvre, 69120 Vaulx-en-Velin</p>
+                                </div>
+
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default AddressSection;
--- src/pages/home-page/home-page.component.jsx
+++ src/pages/home-page/home-page.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavigationBar from "../../components/navigation-bar/navigation-bar.compoenent";
 import Header from "../../components/header/header.compoenent";
+import AddressSection from "../../components/address-section/address-section.component";
 
 const HomePage = () => {
     return (
         <div className="home-page">
            <NavigationBar/>
            <Header/>
+           <AddressSection/>
         </div>
     )
 }
